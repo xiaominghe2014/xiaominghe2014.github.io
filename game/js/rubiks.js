86812745bc26b65f34c94da3e1414b3c278b6229
xiaoming <xiaominghe2014@gmail.com>
2023-07-04 16:29:17 +0800

    fix

--- game/js/rubiks.js
+++ game/js/rubiks.js
@@ -1,3 +1,4 @@
+
 // Create a scene
 const scene = new THREE.Scene();
 
@@ -15,83 +16,106 @@ const smallCubeGeometry = new THREE.BoxGeometry(0.95, 0.95, 0.95);
 
 // Create an array to hold all the small cube meshes
 const smallCubes = [];
+const smallCubeMaterials = [
+    new THREE.MeshBasicMaterial({ color: 0xffff00, shadowSide: THREE.DoubleSide }), // yellow
+    new THREE.MeshBasicMaterial({ color: 0x0000ff, shadowSide: THREE.DoubleSide }), // blue
+    new THREE.MeshBasicMaterial({ color: 0xffffff, shadowSide: THREE.DoubleSide }), // white
+    new THREE.MeshBasicMaterial({ color: 0x00ff00, shadowSide: THREE.DoubleSide }), // green
+    new THREE.MeshBasicMaterial({ color: 0xff0000, shadowSide: THREE.DoubleSide }), // red
+    new THREE.MeshBasicMaterial({ color: 0xffa500, shadowSide: THREE.DoubleSide })  // orange
+];
 
-// Create a cube with different colored faces using small cubes
-// Create a group to hold all the small cubes
+
+
+
+
 const cubeGroup = new THREE.Group();
+var groupX = [new THREE.Group(), new THREE.Group(), new THREE.Group()];
+var groupY = [new THREE.Group(), new THREE.Group(), new THREE.Group()];
 // cubeGroup.scale.set(1,1,1)
 // Create a cube with different colored faces using small cubes
 for (let x = -1; x <= 1; x++) {
     for (let y = -1; y <= 1; y++) {
+
         for (let z = -1; z <= 1; z++) {
-            const smallCubeMaterials = [
-                new THREE.MeshBasicMaterial({ color: 0xffff00, shadowSide: THREE.DoubleSide }), // yellow
-                new THREE.MeshBasicMaterial({ color: 0x0000ff, shadowSide: THREE.DoubleSide }), // blue
-                new THREE.MeshBasicMaterial({ color: 0xffffff, shadowSide: THREE.DoubleSide }), // white
-                new THREE.MeshBasicMaterial({ color: 0x00ff00, shadowSide: THREE.DoubleSide }), // green
-                new THREE.MeshBasicMaterial({ color: 0xff0000, shadowSide: THREE.DoubleSide }), // red
-                new THREE.MeshBasicMaterial({ color: 0xffa500, shadowSide: THREE.DoubleSide })  // orange
-            ];
             const smallCube = new THREE.Mesh(smallCubeGeometry, smallCubeMaterials);
             smallCube.position.set(x, y, z);
             smallCube.receiveShadow = true;
             smallCube.castShadow = true;
+
+            // Load font
+            // const loader = new THREE.FontLoader();
+            // loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
+            //     const textGeometry = new THREE.TextGeometry(`${x},${y},${z}`, {
+            //         font: font,
+            //         size: 0.1,
+            //         height: 0.05,
+            //     });
+            //     const textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+            //     const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+            //     textMesh.position.copy(smallCube.position.clone().add(new THREE.Vector3(0, 0, 0)));
+            //     smallCube.add(textMesh);
+            // });
             cubeGroup.add(smallCube); // Add the small cube to the group
+            // groupX[x + 1].add(smallCube);
+            // groupY[y + 1].add(smallCube);
         }
     }
 }
+
+// for (let i = 0; i < 3; i++) {
+//     cubeGroup.add(groupX[i]);
+//     cubeGroup.add(groupY[i]);
+// }
 
 // Add the cube group to the scene
 scene.add(cubeGroup);
 function animate() {
     requestAnimationFrame(animate);
-    // TWEEN.update();
-    // Rotate the small cubes
+    // const group = new THREE.Group(); // Create a new group
 
-    cubeGroup.children.forEach((cube) => {
-        if (cube.position.x === 0) {
-            cube.rotation.x += 0.02;
-        }
-        // if (cube.position.y === 0) {
-        //     cube.rotation.y += 0.02;
-        // }
-        // if (cube.position.z === 0) {
-        //     cube.rotation.z += 0.02;
-        // }
-    });
+    // Add cubes with x = layerIndex to the group
+    if (groupX[1].children.length == 0) {
+        const layerCubes = cubeGroup.children.filter(cube => cube.position.x === 0);
+        layerCubes.forEach((cube) => {
+            if (cube.position.x === 0) {
+                groupX[1].add(cube);
+            }
+            // console.log(cube.position)
+        });
+
+        // groupX[1].children.forEach((cube) => {
+        //     cubeGroup.remove(cube);
+        // });
+        cubeGroup.add(groupX[1]);
+    }
+
+    // Rotate the group
+    groupX[1].rotation.x += 0.02;
+
+    // Add the group back to the cubeGroup
     cubeGroup.rotation.x += 0.01;
     cubeGroup.rotation.y += 0.01;
     // Render the scene with the camera
     renderer.render(scene, camera);
+
+    // groupX[1].children.forEach((cube) => {
+    //     groupX[1].remove(cube);
+    //     cubeGroup.add(cube);
+    // });
+    // cubeGroup.remove(groupX[1]);
 }
 
-// animate();
+animate();
 renderer.render(scene, camera);
 
 
 function rotateLayerX(layerIndex, direction) {
-    // 根据层级和方向计算旋转角度
-    const angle = direction === 'clockwise' ? Math.PI / 2 : -Math.PI / 2;
 
-    // 遍历指定层级上的小方块，进行旋转
-    cubeGroup.children.forEach((cube) => {
-        if (cube.position.x === layerIndex) {
-            cube.rotation.x = angle;
-        }
-    });
-    renderer.render(scene, camera);
 }
 
 function rotateLayerY(layerIndex, direction) {
-    // 根据层级和方向计算旋转角度
-    const angle = direction === 'clockwise' ? Math.PI / 2 : -Math.PI / 2;
-
-    // 遍历指定层级上的小方块，进行旋转
-    cubeGroup.children.forEach((cube) => {
-        if (cube.position.y === layerIndex) {
-            cube.rotation.y = angle;
-        }
-    });
+    
 }
 
 // rotateLayerX(1,'clockwise');
